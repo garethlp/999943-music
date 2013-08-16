cce00da3df12d72ded027160bb428ab8389ca653
dvdrtrgn <dvdrtrgn@gmail.com>
2013-08-16 14:16:45 -0500

    detect hires

--- scripts/main.js
+++ scripts/main.js
@@ -103,6 +103,31 @@ function Main(W) {
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+Modernizr.addTest('highres', function() {
+    // for opera
+    var ratio = '2.99/2';
+    // for webkit
+    var num = '1.499';
+    var mqs = [
+    'only screen and (-o-min-device-pixel-ratio:' + ratio + ')',
+    'only screen and (min--moz-device-pixel-ratio:' + num + ')',
+    'only screen and (-webkit-min-device-pixel-ratio:' + num + ')',
+    'only screen and (min-device-pixel-ratio:' + num + ')'
+    ];
+    var isHighRes = false;
+
+    // loop through vendors, checking non-prefixed first
+    for (var i = mqs.length - 1; i >= 0; i--) {
+        isHighRes = Modernizr.mq( mqs[i] );
+        // if found one, return early
+        if ( isHighRes ) {
+            return isHighRes;
+        }
+    }
+    // not highres
+    return isHighRes;
+});
+
 /*
 
 
